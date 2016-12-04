b41a4c5bb2a75aa99d587ab4afa0729b822f30d5
Diego Torres <diegotorres50@hotmail.com>
2016-12-04 20:39:09 -0300

    cfg-002 refactor para el constructor slim con con el container, reorganizando el archivo raiz de rutas y renombrando el settings por container.config

--- src/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Routes
-
-require_once __DIR__ . '/../src/routes/test/routes.php';
-require_once __DIR__ . '/../src/routes/test2/routes.php';
---
+++ src/routes/routes.php
@@ -0,0 +1,5 @@
+<?php
+// Routes
+
+require_once __DIR__ . '/../../src/routes/test/routes.php';
+require_once __DIR__ . '/../../src/routes/test2/routes.php';
